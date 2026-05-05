e8eabb6eea8d45478aab4c0d44e8f8464e213c67
shaharban-banu <sonushaharban44@gmail.com>
2026-05-05 14:36:07 +0530

    changed deploy

--- deploy.py
+++ deploy.py
@@ -5,14 +5,11 @@ import boto3
 
 role="arn:aws:iam::285407030131:role/sagemaker_execution_role"
 
-endpoint_name="iris-endpoint"
-config_name = f"{endpoint_name}-config-{int(time.time())}"
-
 boto_session = boto3.Session(region_name="ap-south-1")
 session = sagemaker.Session(boto_session=boto_session)
 
 
-#endpoint_name = f"iris-endpoint-{int(time.time())}"
+endpoint_name = f"iris-endpoint--{int(time.time())}"
 
 model = SKLearnModel(
     model_data="s3://sagemaker-bucket-11/model.tar.gz",
@@ -38,7 +35,6 @@ if endpoint_exists:
         instance_type="ml.t2.medium",
         initial_instance_count=1,
         endpoint_name=endpoint_name,
-        endpoint_config_name=config_name,
         update_endpoint=True
     )
 else:
@@ -46,42 +42,7 @@ else:
     predictor = model.deploy(
         instance_type="ml.t2.medium",
         initial_instance_count=1,
-        endpoint_name=endpoint_name,
-        endpoint_config_name=config_name
+        endpoint_name=endpoint_name
     )
 
 print("✅ Deployment complete")
-
-# sm_client=boto3.client("sagemaker")
-# sm_session=sagemaker.Session()
-
-# #config_name=f"{endpoint_name}--{int(time.time())}"
-
-# def endpoint_exist(name):
-#     try:
-#         sm_client.describe_endpoint(EndpointName=name)
-#         return True
-#     except:
-#         return False
-
-# model=SKLearnModel(
-#     model_data="s3://sagemaker-bucket-11/model.tar.gz",
-#     role=role,
-#     entry_point="inference.py",
-#     framework_version="1.2-1"
-# )
-
-# if endpoint_exist(endpoint_name):
-#     print("updating existing endpoint...")
-
-#     model_name=f"iris-model--{int(time.time())}"
-#     container=model.prepare_container_def()
-
-#     sm_client.creat
-# else:
-#     print("creating new endpoint.....")
-#     predictor=model.deploy(
-#         instance_type="ml.t2.medium",
-#         initial_instance_count=1,
-#         endpoint_name=endpoint_name
-#     )
